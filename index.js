66277182933e48409e3d9b93c1ed49d891724a00
cskmacedo <kleber.barreto@msn.com>
2022-07-16 03:33:15 -0300

    Retornando o primeiro Json

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
+app.set('json spaces', 4);
+
 app.get('/', (req, res) => {
     res.json({status: 'Ntask API'});
+});
+
+app.get('/tasks', (req, res) => {
+    res.json({
+        tasks: [
+            {title: 'Fazer compras'},
+            {title: 'Consertar o pc'},
+        ]
+    });
 });
 
 app.listen(PORT, () => {
