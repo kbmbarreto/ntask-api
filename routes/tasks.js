84eafb26d426d2f3856a5afe199813616d995c4d
cskmacedo <kleber.barreto@msn.com>
2022-08-04 01:44:37 -0300

    Terminado de implementar o jwt

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,9 +2,11 @@ module.exports = app => {
     const Tasks = app.models.tasks;
 
     app.route('/tasks')
+        .all(app.auth.authenticate())
         .get(async (req, res) => {
             try {
-                const result = await Tasks.findAll();
+                const where = { userId: req.user.id };
+                const result = await Tasks.findAll({where});
                 res.json(result);
             } catch (ex) {
                 res.status(412).json({msg: ex.message});
@@ -12,6 +14,7 @@ module.exports = app => {
         })
         .post(async (req, res) => {
             try {
+                req.body.userId = req.user.id;
                 const result = await Tasks.create(req.body);
                 res.json(result);
             } catch (ex) {
@@ -20,9 +23,11 @@ module.exports = app => {
         });
 
     app.route('/tasks/:id')
+        .all(app.auth.authenticate())
         .get(async (req, res) => {
             try {
                 const {id} = req.params;
+                const where = { id, userId: req.user.id };
                 const result = await Tasks.findOne({where});
                 if(result) {
                     res.json(result);
@@ -36,7 +41,8 @@ module.exports = app => {
         .put(async (req, res) => {
             try {
                 const {id} = req.params;
-                const where = {id};
+                const where = {id, userId: req.user.id};
+                req.body.userId = req.user.id;
                 await Tasks.update(req.body, {where});
                 res.sendStatus(204);
             } catch (ex) {
@@ -46,7 +52,7 @@ module.exports = app => {
         .delete(async (req, res) => {
             try {
                 const {id} = req.params;
-                const where = {id};
+                const where = {id, userId: req.user.id};
                 await Tasks.destroy({where});
                 res.sendStatus(204);
             } catch (ex) {
