c4e36d33880976a8f9754623b7023cfa2f7b15ca
cskmacedo <kleber.barreto@msn.com>
2022-08-25 23:53:28 -0300

    Adicionado o cors

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 module.exports = app => {
     app.set('port', 3000);
     app.set('json spaces', 4);
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
